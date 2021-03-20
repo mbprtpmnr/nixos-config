d33dc2969520557882ebfc81742eb63f6db0d938
Norbert Melzer <timmelzer@gmail.com>
2021-03-20 10:43:41 +0100

    change default value in `gc` module

--- modules/gc.nix
+++ modules/gc.nix
@@ -1,16 +1,18 @@
 { config, lib, pkgs, ... }:
 
+let cfg = config.programs.gc;
+in
 {
   options.programs.gc = {
     enable = lib.mkEnableOption "enable GC tool";
 
     maxAge = lib.mkOption {
       type = lib.types.str;
-      default = "180d";
+      default = "10d";
     };
   };
 
-  config = lib.mkIf config.programs.gc.enable {
+  config = lib.mkIf cfg.enable {
     environment.systemPackages = [
       (pkgs.writeShellScriptBin "gc" ''
         sudo ${config.nix.package}/bin/nix-collect-garbage --delete-older-than ${config.programs.gc.maxAge} --verbose
