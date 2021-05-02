35cf328be6fa99bc30bd90c665e463ea96694c93
Norbert Melzer <timmelzer@gmail.com>
2021-05-02 10:18:18 +0200

    restic: add original command invocation to module
    This should help me to build the actual module from and servers me as a reminder how to run restic until the module is ready

--- home/modules/restic/default.nix
+++ home/modules/restic/default.nix
@@ -18,3 +18,5 @@ in
     };
   };
 }
+
+# restic --repo /run/media/nmelzer/data/restic/repo --verbose=10 backup --exclude=/home/nmelzer/Videos --exclude=/home/nmelzer/.{cache,cabal,cargo,config,emacs.d/{eln-cache,.cache},gem,gradle,hex,kube,local,m2,minikube,minishift,mix,mozilla,npm,opam,rancher,vscode-oss} --exclude=/home/nmelzer/Downloads --exclude=/home/nmelzer/VirtualBox\ VMs --exclude=/home/nmelzer/go/pkg --exclude=_build --exclude=deps --exclude=result --exclude=.elixir_ls --exclude=.ccls-cache --exclude=.direnv -x ~
